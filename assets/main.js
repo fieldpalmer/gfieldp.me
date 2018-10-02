7d9f938edd991a90873370de2a256cba6e5a1751
fieldpalmer <gfieldpalmer@gmail.com>
2018-10-02 15:09:40 -0400

    relative link paths fixed

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
-const loadHome = () => {$("#home-page").load("/templates/home-info-content.html");}
-const loadAbout = () => {$("#about-page").load("/templates/about-me.html");}
-const loadPortfolio = () => {$("#portfolio-page").load("/templates/portfolio.html");}
+const loadHome = () => {$("#home-page").load("./templates/home-info-content.html");}
+const loadAbout = () => {$("#about-page").load("./templates/about-me.html");}
+const loadPortfolio = () => {$("#portfolio-page").load("./templates/portfolio.html");}
 // const loadContact = () => {$("#contact-page").load("../templates/contact.html");}
 // const loadStore = () => {$("#shop-page").load("../templates/shop.html");}
 
@@ -21,7 +21,7 @@ $("#top-nav-about-link").on("click", loadAbout);
     //background-slideshow
     let count = 0
     let showImage;
-    let images = ["/assets/images/aspen-maroon-bells.JPG", "/assets/images/bangkok-aquarium-shark-head.jpg",  "/assets/images/delhi-jamid-mosque.JPG", "/assets/images/hampi-elephant-bath.jpg", "/assets/images/hampi-hanuman-boys.jpg", "/assets/images/kuala-lumpur-murugan.jpg", "/assets/images/morocco-rabat-beach-spot.jpeg", "/assets/images/napel-pokhara-canoes.jpg", "/assets/images/nepal-annapurna-village.jpg", "/assets/images/reading-bangkok-infinity-pool.jpg", "/assets/images/red-fort-delhi.jpg", "/assets/images/taj-mahal-agra.jpg", "/assets/images/varanasi-ganges-pier.jpg"]
+    let images = ["./assets/images/aspen-maroon-bells.JPG", "./assets/images/bangkok-aquarium-shark-head.jpg",  "./assets/images/delhi-jamid-mosque.JPG", "./assets/images/hampi-elephant-bath.jpg", "./assets/images/hampi-hanuman-boys.jpg", "./assets/images/kuala-lumpur-murugan.jpg", "./assets/images/morocco-rabat-beach-spot.jpeg", "./assets/images/napel-pokhara-canoes.jpg", "./assets/images/nepal-annapurna-village.jpg", "./assets/images/reading-bangkok-infinity-pool.jpg", "./assets/images/red-fort-delhi.jpg", "./assets/images/taj-mahal-agra.jpg", "./assets/images/varanasi-ganges-pier.jpg"]
     function displayImage() {
         $(".body-content").css("background-image", "url('" + images[count] + "')");
     }

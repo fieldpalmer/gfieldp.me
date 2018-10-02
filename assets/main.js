666a273c3c9455397875bd4d2037e885bb040c2f
fieldpalmer <gfieldpalmer@gmail.com>
2018-10-02 02:22:10 -0400

    relative links fixed

--- assets/main.js
+++ assets/main.js
@@ -21,7 +21,7 @@ $("#top-nav-about-link").on("click", loadAbout);
     //background-slideshow
     let count = 0
     let showImage;
-    let images = ["./assets/images/aspen-maroon-bells.JPG", "./assets/images/bangkok-aquarium-shark-head.jpg",  "./assets/images/delhi-jamid-mosque.JPG", "./assets/images/hampi-elephant-bath.jpg", "./assets/images/hampi-hanuman-boys.jpg", "./assets/images/kuala-lumpur-murugan.jpg", "./assets/images/morocco-rabat-beach-spot.jpeg", "./assets/images/napel-pokhara-canoes.jpg", "./assets/images/nepal-annapurna-village.jpg", "./assets/images/reading-bangkok-infinity-pool.jpg", "./assets/images/red-fort-delhi.jpg", "./assets/images/taj-mahal-agra.jpg", "./assets/images/varanasi-ganges-pier.jpg"]
+    let images = ["../assets/images/aspen-maroon-bells.JPG", "../assets/images/bangkok-aquarium-shark-head.jpg",  "../assets/images/delhi-jamid-mosque.JPG", "../assets/images/hampi-elephant-bath.jpg", "../assets/images/hampi-hanuman-boys.jpg", "../assets/images/kuala-lumpur-murugan.jpg", "../assets/images/morocco-rabat-beach-spot.jpeg", "../assets/images/napel-pokhara-canoes.jpg", "../assets/images/nepal-annapurna-village.jpg", "../assets/images/reading-bangkok-infinity-pool.jpg", "../assets/images/red-fort-delhi.jpg", "../assets/images/taj-mahal-agra.jpg", "./assets/images/varanasi-ganges-pier.jpg"]
     function displayImage() {
         $(".body-content").css("background-image", "url('" + images[count] + "')");
     }

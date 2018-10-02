09b467b8b46aede94cd0450213a8cbb69917381e
fieldpalmer <gfieldpalmer@gmail.com>
2018-10-02 01:38:31 -0400

    new photos

--- assets/main.js
+++ assets/main.js
@@ -7,21 +7,27 @@ const loadPortfolio = () => {$("#portfolio-page").load("../templates/portfolio.h
 //initialize site
 $(document).ready(function(){
     startSlider();
+    loadHome();
     loadPortfolio();
     loadAbout();
 });
+
 //home page content action
+
+$("#top-nav-home-link").on("click", loadHome);
+$("#top-nav-portfolio-link").on("click", loadPortfolio);
+$("#top-nav-about-link").on("click", loadAbout);
 
     //background-slideshow
     let count = 0
     let showImage;
-    let images = ["./assets/images/hanuman-boys-playing-hampi.jpg", "./assets/images/ocean.jpg","./assets/images/rabat-beach-field-friends.jpg","./assets/images/field-france-mont-st-michel.jpg"]
+    let images = ["./assets/images/aspen-maroon-bells.JPG", "./assets/images/bangkok-aquarium-shark-head.jpg",  "./assets/images/delhi-jamid-mosque.JPG", "./assets/images/hampi-elephant-bath.jpg", "./assets/images/hampi-hanuman-boys.jpg", "./assets/images/kuala-lumpur-murugan.jpg", "./assets/images/morocco-rabat-beach-spot.jpeg", "./assets/images/napel-pokhara-canoes.jpg", "./assets/images/nepal-annapurna-village.jpg", "./assets/images/reading-bangkok-infinity-pool.jpg", "./assets/images/red-fort-delhi.jpg", "./assets/images/taj-mahal-agra.jpg", "./assets/images/varanasi-ganges-pier.jpg"]
     function displayImage() {
         $(".body-content").css("background-image", "url('" + images[count] + "')");
     }
     function nextImage() {
         count++;
-        setTimeout(displayImage);
+        setTimeout(displayImage, 1000);
         if (count === images.length) {
             count = 0;
         }

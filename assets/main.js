68433244e981f7f23d762dafb857218b47ada176
fieldpalmer <gfieldpalmer@gmail.com>
2018-10-03 14:10:51 -0400

    trying again

--- assets/main.js
+++ assets/main.js
@@ -1,49 +1,46 @@
 const loadHome = () => {
-    $("#home-page").load("./templates/homeInfoContent.html");
+    $(".content-input").load("./templates/homeInfoContent.html");
 }
 const loadAbout = () => {
-    $("#about-page").load("./templates/aboutMe.html");
+    $(".content-input").load("./templates/aboutMe.html");
 }
 const loadPortfolio = () => {
-    $("#portfolio-page").load("./templates/portfolio.html");
+    $(".content-input").load("./templates/portfolio.html");
 }
 // const loadContact = () => {$("#contact-page").load("../templates/contact.html");}
 // const loadStore = () => {$("#shop-page").load("../templates/shop.html");}
 
 //initialize site
-$(".content-input").html(loadHome);
+$(".content-input").html(loadPortfolio);
+// $(".content-input").append(loadAbout);
+// $(".content-input").append(loadPortfolio);
+// .append(loadAbout() + loadPortfolio());
 
 //home page content action
     //top-nav-menu
-    $("#top-nav-home-link").on("click", function() {
-        $(".content-input").html(loadHome);
-    });
-
-    $("#top-nav-portfolio-link").on("click", function() {
-        $(".content-input").html(loadPortfolio);
-    });
-    $("#top-nav-about-link").on("click", function() {
-        $(".content-input").html(loadAbout);
-    });
+    $("#top-nav-home-link").on("click", loadHome);
+    $("#top-nav-portfolio-link").on("click", loadPortfolio);
+    $("#top-nav-about-link").on("click", loadAbout);
+    
 
     //background-slideshow
-    let count = 0
-    let showImage;
-    let images = ["assets/images/aspen-maroon-bells.JPG", "assets/images/bangkok-aquarium-shark-head.jpg",  "assets/images/delhi-jamid-mosque.JPG", "assets/images/hampi-elephant-bath.jpg", "assets/images/hampi-hanuman-boys.jpg", "assets/images/kuala-lumpur-murugan.jpg", "assets/images/morocco-rabat-beach-spot.jpeg", "assets/images/napel-pokhara-canoes.jpg", "assets/images/nepal-annapurna-village.jpg", "assets/images/reading-bangkok-infinity-pool.jpg", "assets/images/red-fort-delhi.jpg", "assets/images/taj-mahal-agra.jpg", "assets/images/varanasi-ganges-pier.jpg"]
+    // let count = 0
+    // let showImage;
+    // let images = ["assets/images/aspen-maroon-bells.JPG", "assets/images/bangkok-aquarium-shark-head.jpg",  "assets/images/delhi-jamid-mosque.JPG", "assets/images/hampi-elephant-bath.jpg", "assets/images/hampi-hanuman-boys.jpg", "assets/images/kuala-lumpur-murugan.jpg", "assets/images/morocco-rabat-beach-spot.jpeg", "assets/images/napel-pokhara-canoes.jpg", "assets/images/nepal-annapurna-village.jpg", "assets/images/reading-bangkok-infinity-pool.jpg", "assets/images/red-fort-delhi.jpg", "assets/images/taj-mahal-agra.jpg", "assets/images/varanasi-ganges-pier.jpg"]
 
-    function displayImage() {
-        $(".body-content").css("background-image", "url('" + images[count] + "')");
-    }
-    function nextImage() {
-        count++;
-        setTimeout(displayImage, 1000);
-        if (count === images.length) {
-            count = 0;
-        }
-    }
-    function startSlider () { 
-        displayImage();
-        showImage = setInterval(nextImage, 1000 * 13) 
-    }
-    startSlider();
+    // function displayImage() {
+    //     $(".body-content").css("background-image", "url('" + images[count] + "')");
+    // }
+    // function nextImage() {
+    //     count++;
+    //     setTimeout(displayImage, 1000);
+    //     if (count === images.length) {
+    //         count = 0;
+    //     }
+    // }
+    // function startSlider () { 
+    //     displayImage();
+    //     showImage = setInterval(nextImage, 1000 * 13) 
+    // }
+    // startSlider();
     

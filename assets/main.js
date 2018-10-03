4d3676fb4e6ad5d67f657bcbbe52f1267bdcf37d
fieldpalmer <gfieldpalmer@gmail.com>
2018-10-03 14:48:11 -0400

    slider polishing

--- assets/main.js
+++ assets/main.js
@@ -18,19 +18,24 @@ $(".content-input").html(loadPortfolio);
 
 //home page content action
     //top-nav-menu
-    $("#top-nav-home-link").on("click", loadHome);
-    $("#top-nav-portfolio-link").on("click", loadPortfolio);
-    $("#top-nav-about-link").on("click", loadAbout);
-    
+    $(document).on("click", "#top-nav-home-link", loadHome);
+    $(document).on("click", "#top-nav-portfolio-link", loadPortfolio);
+    $(document).on("click", "#top-nav-about-link", loadAbout);
 
     //background-slideshow
     let count = 0
     let showImage;
     let images = ["assets/images/aspen-maroon-bells.JPG", "assets/images/bangkok-aquarium-shark-head.jpg",  "assets/images/delhi-jamid-mosque.JPG", "assets/images/hampi-elephant-bath.jpg", "assets/images/hampi-hanuman-boys.jpg", "assets/images/kuala-lumpur-murugan.JPG", "assets/images/morocco-rabat-beach-spot.jpeg", "assets/images/napel-pokhara-canoes.jpg", "assets/images/nepal-annapurna-village.JPG", "assets/images/reading-bangkok-infinity-pool.jpg", "assets/images/red-fort-delhi.JPG", "assets/images/taj-mahal-agra.JPG", "assets/images/varanasi-ganges-pier.JPG"]
 
+    function startSlider () { 
+        // displayImage();
+        showImage = setInterval(nextImage, 1000 * 13) 
+    }
+    
     function displayImage() {
         $(".body-content").css("background-image", "url('" + images[count] + "')");
     }
+    
     function nextImage() {
         displayImage();
         count++;
@@ -39,9 +44,6 @@ $(".content-input").html(loadPortfolio);
             count = 0;
         }
     }
-    function startSlider () { 
-        displayImage();
-        showImage = setInterval(nextImage, 1000 * 13) 
-    }
+    
     startSlider();
     

9e2f52ffa87cc433ed8c9058596e318c6cf68efc
fieldpalmer <gfieldpalmer@gmail.com>
2018-10-03 14:33:30 -0400

    slideshow working

--- assets/main.js
+++ assets/main.js
@@ -24,23 +24,23 @@ $(".content-input").html(loadPortfolio);
     
 
     //background-slideshow
-    // let count = 0
-    // let showImage;
-    // let images = ["assets/images/aspen-maroon-bells.JPG", "assets/images/bangkok-aquarium-shark-head.jpg",  "assets/images/delhi-jamid-mosque.JPG", "assets/images/hampi-elephant-bath.jpg", "assets/images/hampi-hanuman-boys.jpg", "assets/images/kuala-lumpur-murugan.jpg", "assets/images/morocco-rabat-beach-spot.jpeg", "assets/images/napel-pokhara-canoes.jpg", "assets/images/nepal-annapurna-village.jpg", "assets/images/reading-bangkok-infinity-pool.jpg", "assets/images/red-fort-delhi.jpg", "assets/images/taj-mahal-agra.jpg", "assets/images/varanasi-ganges-pier.jpg"]
+    let count = 0
+    let showImage;
+    let images = ["assets/images/aspen-maroon-bells.JPG", "assets/images/bangkok-aquarium-shark-head.jpg",  "assets/images/delhi-jamid-mosque.JPG", "assets/images/hampi-elephant-bath.jpg", "assets/images/hampi-hanuman-boys.jpg", "assets/images/kuala-lumpur-murugan.jpg", "assets/images/morocco-rabat-beach-spot.jpeg", "assets/images/napel-pokhara-canoes.jpg", "assets/images/nepal-annapurna-village.jpg", "assets/images/reading-bangkok-infinity-pool.jpg", "assets/images/red-fort-delhi.jpg", "assets/images/taj-mahal-agra.jpg", "assets/images/varanasi-ganges-pier.jpg"]
 
-    // function displayImage() {
-    //     $(".body-content").css("background-image", "url('" + images[count] + "')");
-    // }
-    // function nextImage() {
-    //     count++;
-    //     setTimeout(displayImage, 1000);
-    //     if (count === images.length) {
-    //         count = 0;
-    //     }
-    // }
-    // function startSlider () { 
-    //     displayImage();
-    //     showImage = setInterval(nextImage, 1000 * 13) 
-    // }
-    // startSlider();
+    function displayImage() {
+        $(".body-content").css("background-image", "url('" + images[count] + "')");
+    }
+    function nextImage() {
+        count++;
+        setTimeout(displayImage, 1000);
+        if (count === images.length) {
+            count = 0;
+        }
+    }
+    function startSlider () { 
+        displayImage();
+        showImage = setInterval(nextImage, 1000 * 13) 
+    }
+    startSlider();
     

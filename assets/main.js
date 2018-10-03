b8ee4e1537605af7492068e9ece1a78e60b5a656
fieldpalmer <gfieldpalmer@gmail.com>
2018-10-03 11:46:14 -0400

    photo links fixed

--- assets/main.js
+++ assets/main.js
@@ -29,7 +29,7 @@ $(".content-input").html(loadHome);
     //background-slideshow
     let count = 0
     let showImage;
-    let images = ["./images/aspen-maroon-bells.JPG", "./images/bangkok-aquarium-shark-head.jpg",  "./images/delhi-jamid-mosque.JPG", "./images/hampi-elephant-bath.jpg", "./images/hampi-hanuman-boys.jpg", "./images/kuala-lumpur-murugan.jpg", "./images/morocco-rabat-beach-spot.jpeg", "./images/napel-pokhara-canoes.jpg", "./images/nepal-annapurna-village.jpg", "./images/reading-bangkok-infinity-pool.jpg", "./images/red-fort-delhi.jpg", "./images/taj-mahal-agra.jpg", "./images/varanasi-ganges-pier.jpg"]
+    let images = ["/images/aspen-maroon-bells.JPG", "/images/bangkok-aquarium-shark-head.jpg",  "/images/delhi-jamid-mosque.JPG", "/images/hampi-elephant-bath.jpg", "/images/hampi-hanuman-boys.jpg", "/images/kuala-lumpur-murugan.jpg", "/images/morocco-rabat-beach-spot.jpeg", "/images/napel-pokhara-canoes.jpg", "/images/nepal-annapurna-village.jpg", "/images/reading-bangkok-infinity-pool.jpg", "/images/red-fort-delhi.jpg", "/images/taj-mahal-agra.jpg", "/images/varanasi-ganges-pier.jpg"]
     function displayImage() {
         $(".body-content").css("background-image", "url('" + images[count] + "')");
     }
